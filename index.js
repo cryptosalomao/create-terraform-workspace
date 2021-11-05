5b68ecfe9c47b69753e0f07b603e22c40e326161
Guilherme Salomão <cryptosalomao@gmail.com>
2021-11-05 03:42:29 -0300

    update script to set aws variables automatically

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ try{
     const organizationName = core.getInput('organizationName');
     const token = core.getInput('terraformToken');
     const terraformHost = core.getInput('terraformHost');
+
+    const awsAccessKeyId = core.getInput('awsAccessKeyId');
+    const awsSecretAccessKey = core.getInput('awsSecretAccessKey');
+
+    const setAccessKeyRequest = { data: { type: "vars", attributes: { key: "AWS_ACCESS_KEY_ID", value: awsAccessKeyId, category: "env", sensitive: true } } }
+    const setSecretAccessKeyRequest = { data: { type: "vars", attributes: { key: "AWS_SECRET_ACCESS_KEY", value: awsSecretAccessKey, category: "env", sensitive: true } } }
+
     let request = { data : { attributes: { name : workSpaceName, type: "workspaces"}}};
     console.log("request:" + JSON.stringify(request));
 
@@ -17,8 +24,12 @@ try{
     };
    // Invoking Terraform API
     axios.post(terraformEndpoint, request, options)
-      .then((response) => {
+      .then(async (response) => {
         console.log("success:"+ JSON.stringify(response.data));
+
+        await axios.post(`${terraformEndpoint}/${response.data.data.id}/vars`, setAccessKeyRequest, options)
+        await axios.post(`${terraformEndpoint}/${response.data.data.id}/vars`, setSecretAccessKeyRequest, options)
+
         core.setOutput("workSpaceId", response.data.data.id);
       }, (error) => {
         console.error("error:"+JSON.stringify(error.response.data));
